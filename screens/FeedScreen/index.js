ce281ae39efc64049a4f337fbf166eb00f4252e6
junngo <myeongjun.ko@gmail.com>
2020-02-26 20:47:03 +0900

    making the feedscreen(using scrollview, refreshControl / working redux(feed))

--- screens/FeedScreen/index.js
+++ screens/FeedScreen/index.js
@@ -1,4 +1,22 @@
 import { connect } from "react-redux";
 import Container from "./container";
+import { actionCreators as photoActions } from "../../redux/modules/photo";
 
-export default connect()(Container);
+
+const mapStateToProps = (state, ownProps) => {
+    const { photo: { feed } } = state;
+    console.log(feed);
+    return {
+        feed
+    };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        getFeed: () => {
+            dispatch(photoActions.getFeed());
+        }
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Container);
